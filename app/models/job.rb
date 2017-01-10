8e7dc02fd105ad5c93cf10b70816735b5162eb95
zhoujin <jin100chow@hotmail.com>
2017-01-10 17:08:43 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,11 @@
 class Job < ApplicationRecord
+  has_many :resumes
+
   scope :published, -> { where(is_hidden: false) }
 
 
   scope :recent, -> { order('created_at DESC') }
-   
+
 
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
