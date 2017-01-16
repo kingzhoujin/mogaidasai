1054d5aba366dd0591be2f29b30114734ef0f336
zhoujin <jin100chow@hotmail.com>
2017-01-17 00:03:06 +0800

    modified page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   resources :welcome do
+   end
    resources :jobs do
      resources :resumes
     end
-    root 'jobs#index'
+    root 'welcome#index'
 
 
    namespace :admin do
